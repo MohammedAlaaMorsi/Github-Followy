d2c63ac89853aaec69b6f88849faf5e76b27c99a
Mohammed Alaa <zicoreal@ymail.com>
2025-12-09 21:14:44 +0200

    Initialize renovate/sersoft-gmbh-xcodebuild-action-4.x

---
+++ iosApp/templateIOS/templateIOSApp.swift
@@ -0,0 +1,15 @@
+//
+//  templateIOSApp.swift
+//  templateIOS
+//
+
+import SwiftUI
+
+@main
+struct templateIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
